41198c69f2109f7d887a4538e121f22d7082c634
Boitumelo Iven Moshitwa <itumelo553@gmail.com>
2024-03-11 19:33:55 +0200

    add the thank you page with its routing and store data from contact form in csv/text file and also configured the contact form

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import csv
+from flask import Flask, render_template,url_for,request,redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,5 +11,29 @@ def my_home():
 def html_pages(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        file = database.write(f"\n {email}, {subject}, {message}")
 
 
+def write_to_csv(data):
+     with open('database.csv', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        csv_witer = csv.writer(database2,delimiter=" , ", newline='', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        csv_witer.writerow([email,subject,message])
+
+@app.route('/submit_form',methods=['POST','GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect("/thankyou.html")
+    else:
+        return 'Something Went Wrong'
